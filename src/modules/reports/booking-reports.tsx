9009a3b4d435042c3599cab0d50c605125500c73
Hashim Hashimli <hashimli.hshm@gmail.com>
2024-06-16 18:39:35 +0400

    changes

--- src/modules/reports/booking-reports.tsx
+++ src/modules/reports/booking-reports.tsx
@@ -11,10 +11,11 @@ import {
   TableRow
 } from '@nextui-org/react';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Empty from '@/components/layout/empty';
 import { MdRefresh, MdSearch } from 'react-icons/md';
 import { RoomsService } from '@/services/rooms-services/rooms-services';
+import { FaSort } from 'react-icons/fa';
 import { format, parseISO } from 'date-fns';
 import AppHandledBorderedButton from '@/components/forms/button/app-handled-bordered-button';
 import AppHandledSolidButton from '@/components/forms/button/app-handled-solid-button';
@@ -28,10 +29,17 @@ import AppHandledSelect from '@/components/forms/select/handled-select';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { convertFormDataToQueryParams } from '@/utils/functions/functions';
 import { IHTTPSParams } from '@/services/adapter-config/config';
-import { IBookingReportsResponse, IReportFilter } from './types';
+import { IBookingReportsResponse, IReportFilter, IReportItem } from './types';
 
 function convertDateToISO(inputDate: string): any {
   return format(inputDate, "yyyy-MM-dd'T'HH:mm");
+}
+
+type SortDirection = 'ascending' | 'descending';
+
+interface SortConfig {
+  key: keyof IReportItem | null;
+  direction: SortDirection;
 }
 
 function LeadsTable() {
@@ -40,6 +48,10 @@ function LeadsTable() {
   const [tableLoading, setTableLoading] = useState(true);
   const [queryParams, setQueryParams] = useState<IHTTPSParams[]>([]);
   const [refreshComponent, setRefreshComponent] = useState<boolean>(false);
+  const [sortConfig, setSortConfig] = useState<SortConfig>({
+    key: null,
+    direction: 'ascending'
+  });
 
   const {
     handleSubmit,
@@ -71,6 +83,7 @@ function LeadsTable() {
     setQueryParams(queryParamsData);
     setRefreshComponent(!refreshComponent);
   };
+
   async function getReports() {
     setTableLoading(true);
 
@@ -90,26 +103,55 @@ function LeadsTable() {
 
   useEffect(() => {
     getReports();
-  }, [currentPage, refreshComponent]);
+  }, [currentPage, refreshComponent, sortConfig]);
+
+  const sortedItems = useMemo(() => {
+    const sortableItems = [...(data?.items || [])];
+    if (sortConfig.key !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [data, sortConfig]);
+
+  const requestSort = (key: keyof IReportItem) => {
+    // Ensure direction is always a valid SortDirection type
+    let direction: SortDirection = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
 
   const renderRows = () =>
-    data?.items?.map(item => (
-      <TableRow className="cursor-pointer" key={item?.bookingId}>
-        <TableCell>{item?.deskName || '-'}</TableCell>
-        <TableCell>{item?.deskOwnerName || '-'}</TableCell>
-        <TableCell>{item?.roomName || '-'}</TableCell>
+    sortedItems.map((item: IReportItem) => (
+      <TableRow className="cursor-pointer" key={item.bookingId}>
+        <TableCell>{item.deskName || '-'}</TableCell>
+        <TableCell>{item.deskOwnerName || '-'}</TableCell>
+        <TableCell>{item.roomName || '-'}</TableCell>
         <TableCell>
-          {item?.operationType === 1
+          {item.operationType === 1
             ? 'Booking'
-            : item?.operationType === 2
+            : item.operationType === 2
             ? 'Cancel'
             : '-'}
         </TableCell>
         <TableCell>
-          {format(parseISO(item?.startDate), 'dd.MM.yyyy HH:mm') || '-'}
+          {item.startDate
+            ? format(parseISO(item.startDate), 'dd.MM.yyyy HH:mm')
+            : '-'}
         </TableCell>
         <TableCell>
-          {format(parseISO(item?.endDate), 'dd.MM.yyyy HH:mm') || '-'}
+          {item.endDate
+            ? format(parseISO(item.endDate), 'dd.MM.yyyy HH:mm')
+            : '-'}
         </TableCell>
       </TableRow>
     ));
@@ -272,12 +314,60 @@ function LeadsTable() {
             }
           >
             <TableHeader>
-              <TableColumn>Desk name</TableColumn>
-              <TableColumn>Desk owner</TableColumn>
-              <TableColumn>Room Name</TableColumn>
-              <TableColumn>Operation type</TableColumn>
-              <TableColumn>Start Date</TableColumn>
-              <TableColumn>End Date</TableColumn>
+              <TableColumn
+                className="cursor-pointer"
+                onClick={() => requestSort('deskName')}
+              >
+                <p className="flex items-center gap-2">
+                  {' '}
+                  Desk name <FaSort />
+                </p>
+              </TableColumn>
+              <TableColumn
+                className="cursor-pointer"
+                onClick={() => requestSort('deskOwnerName')}
+              >
+                <p className="flex items-center gap-2">
+                  {' '}
+                  Desk owern <FaSort />
+                </p>
+              </TableColumn>
+              <TableColumn
+                className="cursor-pointer"
+                onClick={() => requestSort('roomName')}
+              >
+                <p className="flex items-center gap-2">
+                  {' '}
+                  Room name <FaSort />
+                </p>
+              </TableColumn>
+              <TableColumn
+                className="cursor-pointer"
+                onClick={() => requestSort('operationType')}
+              >
+                <p className="flex items-center gap-2">
+                  {' '}
+                  Operation type <FaSort />
+                </p>
+              </TableColumn>
+              <TableColumn
+                className="cursor-pointer"
+                onClick={() => requestSort('startDate')}
+              >
+                <p className="flex items-center gap-2">
+                  {' '}
+                  Start date <FaSort />
+                </p>
+              </TableColumn>
+              <TableColumn
+                className="cursor-pointer"
+                onClick={() => requestSort('endDate')}
+              >
+                <p className="flex items-center gap-2">
+                  {' '}
+                  End date <FaSort />
+                </p>
+              </TableColumn>
             </TableHeader>
             <TableBody
               emptyContent={<Empty />}
